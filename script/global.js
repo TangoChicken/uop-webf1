eabd6e7c6854be51365f2c616e1816d87c09e02f
TangoChicken <oliverplatt123@gmail.com>
2018-02-14 21:27:15 +0000

    Added tab support in the navigation

--- script/global.js
+++ script/global.js
@@ -1,7 +1,5 @@
 const para = document.getElementsByClassName("parallax-container")[0];
 
-document.body.onscroll = function() {
-   // if(document.body.scrollHeight <= para.scrollHeight / 2){
-        para.scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    //}
-  };
+document.body.onscroll = function () {
+  para.scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+};
