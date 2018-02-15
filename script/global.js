8b8f61c47741ab70b64a747ce0e3d69356d9dd9a
TangoChicken <oliverplatt123@gmail.com>
2018-02-15 00:22:11 +0000

    Added js workaround for no focus-within in Edge

--- script/global.js
+++ script/global.js
@@ -1,5 +1,39 @@
-const para = document.getElementsByClassName("parallax-container")[0];
+document.addEventListener('scroll', function () {
+    let para = document.getElementsByClassName("parallax-container")[0];
+    let scroll = window.pageYOffset || document.documentElement.scrollTop;
+    if (scroll <= para.scrollHeight) {
+        para.scrollTop = scroll;
+    }
+});
 
-document.body.onscroll = function () {
-  para.scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-};
+function supportsFocusWithin() {
+    try {
+        let ss = document.styleSheets[0]
+        ss.insertRule('html:focus-within', 0);
+        ss.deleteRule(0);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+function focusClass(e, f) {
+    e.cancelBubble = true;
+    if (typeof e.srcElement !== 'undefined' && e.srcElement !== null) {
+        let p = e.srcElement.closest("ul");
+        if (p.classList.contains("nav-flyout")) {
+            f ? p.classList.add("nav-focus") : p.classList.remove("nav-focus");
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (!supportsFocusWithin()) {
+        document.addEventListener('blur', function (e) {
+            focusClass(e, false);
+        }, true);
+        document.addEventListener('focus', function (e) {
+            focusClass(e, true);
+        }, true);
+    }
+});
